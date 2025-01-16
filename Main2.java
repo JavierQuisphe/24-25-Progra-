b1efecbe3ea7c53318c3a4b3a1f7063f3ed3b140
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2025-01-17 00:19:16 +0100

    String practica

--- Main2.java
+++ Main2.java
@@ -1,3 +1,23 @@
 public class Main2 {
-    
+    public static void main(String[] args) {
+        String text = "Hola Mundo";
+
+        int largo = text.length();
+        System.out.println(largo);
+
+        String texto = text.replace("Hola", "Chao");
+        System.out.println(texto);
+
+        System.out.println(texto.endsWith("undo!"));
+        System.out.println(texto.startsWith("Ch"));
+        System.out.println(texto.contains("und"));
+        System.out.println(texto.indexOf("Mund"));
+
+        System.out.println(texto.toUpperCase());
+        System.err.println(texto.toLowerCase());
+
+        String malo = "      Chanchito Feliz        ";
+        System.out.println(malo.trim());
+        System.out.println(malo);
+    }
 }
