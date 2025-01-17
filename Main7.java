effab6b0be2955bc2257952afee73abfa80bf01e
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2025-01-17 03:33:26 +0100

    for

---
+++ Main7.java
@@ -0,0 +1,9 @@
+public class Main7 {
+    public static void main(String[] args) {
+        String[] textos = { "Hola Mundo", "Usuario1", "Chanchito Feliz"};
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hola Mundo" + i);
+        }
+    }
+}
