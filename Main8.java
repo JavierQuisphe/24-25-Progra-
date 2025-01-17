eb3db8774c627703ea4965d583e584962763a7a2
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2025-01-17 03:37:52 +0100

    While

---
+++ Main8.java
@@ -0,0 +1,12 @@
+public class Main8 {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 5) { 
+            System.out.println("Hola Mundo" + i);
+            i++;
+        }
+
+        System.out.println("Saliendo del loop");
+    }
+}
