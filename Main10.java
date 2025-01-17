ccd84b7edfbfd2b87fd2ed88aa6c8630d71c62b9
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2025-01-17 03:54:18 +0100

    Metodos

---
+++ Main10.java
@@ -0,0 +1,17 @@
+public class Main10 {
+    public static void main(String[] args) {
+        miMetodo();
+        miMetodo();
+        miMetodo();
+        otroMetodo();
+        otroMetodo();
+    }
+
+    static void miMetodo() { 
+        System.out.println("Hola Mundo");
+    }
+
+    static void otroMetodo() {
+        System.out.println("Chanchito Feliz");
+    }
+}
